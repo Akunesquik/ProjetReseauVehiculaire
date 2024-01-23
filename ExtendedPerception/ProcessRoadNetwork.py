a08376d0af8562600250cb5b05f8c0fcc4baa164
alexis.fiolet <alexis.fiolet@gmail.com>
2024-01-23 16:38:35 +0100

    Ajout de beau print pour chaque object et chaque timestamp

--- ExtendedPerception/ProcessRoadNetwork.py
+++ ExtendedPerception/ProcessRoadNetwork.py
@@ -5,9 +5,8 @@ import pandas
 import pandas as pd 
 import json
 ##from pandas.io.json import json_normalize
-import os, sys
+import os, sys, json
 
-# Made by Louis le boss
 class ExtendedObject:
     def __init__(self, ext_obj_entry):
         self.PerceivedObjectID = ext_obj_entry.get("PerceivedObjectID")
@@ -22,7 +21,7 @@ class ExtendedObject:
 class DataObject:
     def __init__(self, data_entry):
         self.id = data_entry.get("myStationId")
-        self.timestamps = [entry.get("timestamp") for entry in data_entry.get("EP", [])]
+        self.timestamps = data_entry.get("timestamp")
         self.x = data_entry.get("Xegosumoposition")
         self.y = data_entry.get("Yegopsumoposition")
         self.extended_objects = self.extract_extended_objects(data_entry)
@@ -35,51 +34,19 @@ class DataObject:
         return extended_objects
 
     def __str__(self):
-        return f"Classe: {self.id}, Timestamps: {self.timestamps}, X: {self.x}, Y: {self.y}, Extended Objects: {self.extended_objects}"
-# Il a crée deux classes
+        extended_objects_str = f",\n ".join([str(obj) for obj in self.extended_objects])
+        return f"\nClasse: {self.id}, Timestamp: {self.timestamps}, X: {self.x}, Y: {self.y}, Extended Objects: {extended_objects_str}"
 
-""" xtree = et.parse('IRTSystemX.net.xml')
-root = xtree.getroot()
-
-df_cols = ["idjunc", "typejunc", "xjunc", "yjunc", "incLanes", "intLanes"]
-rows = []
-
-for element in root.iter(tag='junction'):
-    
-    idjunc = element.attrib['id']
-        
-    typejunc = element.attrib['type']
-    posx = element.attrib['x']
-    posy = element.attrib['y']
-    inclanejunc = element.attrib['incLanes']
-    inclanejunc = list(inclanejunc.split())
-    intlanejunc = element.attrib['intLanes']
-    intlanejunc = list(intlanejunc.split())
-    rows.append({"idjunc": idjunc, "typejunc": typejunc, "xjunc":posx , "yjunc":posy,
-                 "incLanes": inclanejunc, "intLanes": intlanejunc})
-    junction_df = pd.DataFrame(rows, columns = df_cols)
-
-        #juncID = junction.text
-        #print (juncID, end=' ')
-        #for junction in element.iter():
-            #junctionID = junction.text
-            #if (junction.tag=='junction'):
-                #print (junction.text)
-print (junction_df) """
 
 ###################################################################################################################################
-"""
-with open('C:\\Users\\Louis\\Downloads\\miiproet_1_2\\ExtendedPerception\\Genuine\\extp_attackrate_0.100000_807.json', 'r') as file:
-    data = json.load(file)
 
-result=[]
+with open('ExtendedPerception\\Genuine\\extp_attackrate_0.100000_807.json', 'r') as file:
+    json_data = json.load(file)
 
-# Vérifier si la clé "EP" existe dans le JSON
-if "EP" in data:
-    # Parcourir chaque objet dans la liste "EP"
-    for entry in data["EP"]:
-        # Vérifier si la clé "timestamp" existe dans l'objet
-        timestamps.append(entry["timestamp"])
+data_objects= []
+for entry in json_data.get("EP", []):
+    data_objects.append(DataObject(entry)) 
 
-
-print(timestamps)"""
+# Afficher le résultat
+for obj in data_objects:
+    print(str(obj))
